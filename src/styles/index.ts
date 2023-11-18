97801a1e8c9ea908eb6208e5b03c9c70f4b42e76
FabianHenrick <fabianhenrick@gmail.com>
2023-11-17 23:13:58 -0300

    Add Stitches for css

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  theme,
+  css,
+  createTheme,
+  getCssText,
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: "#8257e6",
+    },
+  },
+});
